9efaf7ebfc9d75889e8dff50d0bac75b9363f91a
sram-narayan <35987861+sram-narayan@users.noreply.github.com>
2018-03-12 19:33:59 -0500

    Update create_ffmpeg_cmd.sh

--- create_ffmpeg_cmd.sh
+++ create_ffmpeg_cmd.sh
@@ -11,8 +11,8 @@ INPUT=$1
 OUTPUT=$2
 
 # Check if codecs are already in the format supported by Chromecast devices (aac for audio and h264 for video)
-AUDIO_RESULT="$(ffprobe -v error -select_streams a:0 -show_entries stream=codec_name   -of default=noprint_wrappers=1:nokey=1 $INPUT)"
-VIDEO_RESULT="$(ffprobe -v error -select_streams v:0 -show_entries stream=codec_name   -of default=noprint_wrappers=1:nokey=1 $INPUT)"
+AUDIO_RESULT="$(avprobe -v error -select_streams a:0 -show_entries stream=codec_name   -of default=noprint_wrappers=1:nokey=1 $INPUT)"
+VIDEO_RESULT="$(avprobe -v error -select_streams v:0 -show_entries stream=codec_name   -of default=noprint_wrappers=1:nokey=1 $INPUT)"
 
 
 if [ "$AUDIO_RESULT" != "aac" ]; then
@@ -32,6 +32,6 @@ if [ "$INPUT" == rtsp://* ]; then
   IS_RTSP="-rtsp_transport tcp"
 fi
 
-FFMPEG_CMD="ffmpeg -rtsp_transport tcp -i ${INPUT} ${LACKING_AUDIO} -acodec ${DEFAULT_AUDIO} -vcodec ${DEFAULT_VIDEO} -hls_wrap 40 -flags -global_header ${OUTPUT_PATH}${OUTPUT}.m3u8"
+FFMPEG_CMD="avconv -rtsp_transport tcp -i ${INPUT} ${LACKING_AUDIO} -acodec ${DEFAULT_AUDIO} -vcodec ${DEFAULT_VIDEO} -hls_wrap 40 -flags -global_header ${OUTPUT_PATH}${OUTPUT}.m3u8"
 
 echo "${FFMPEG_CMD}"
